45ca77ad33ef258a8d7bcd89caba9a1c429b6031
xujie <xuujie@gmail.com>
2019-07-02 16:39:55 +0800

    edit sort module

--- sort/src/test/java/com/xujie/sort/SortTest.java
+++ sort/src/test/java/com/xujie/sort/SortTest.java
@@ -1,18 +1,26 @@
 package com.xujie.sort;
 
-public class SortApplication {
-    public static void main(String[] args) {
-        int[] array = new int[]{23, 18, 1, 4, 6, 2, 19, 22, 5, 3, 8};
+import org.junit.Test;
 
-        Sorter sorter = new Sorter();
+import static org.junit.Assert.assertArrayEquals;
+
+public class SortTest {
+
+    @Test
+    public void testBubbleSort() {
+        int[] array = new int[]{23, 18, 1, 4, 6, 2, 19, 22, 5, 3, 8};
+        int[] sorted = new int[]{1, 2, 3, 4, 5, 6, 8, 18, 19, 22, 23};
+
+        BubbleSorter sorter = new BubbleSorter(array);
         long t = System.currentTimeMillis();
         System.out.print("before:\t\t");
         sorter.print(array);
 
-        sorter.bubbleSort(array);
+        sorter.sort();
 
         System.out.print("after:\t\t");
         sorter.print(array);
         System.out.println("time spent: " + (System.currentTimeMillis() - t));
+        assertArrayEquals(array, sorted);
     }
 }
